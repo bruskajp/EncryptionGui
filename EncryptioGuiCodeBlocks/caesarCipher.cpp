7c76824e22d7a477ba9892ad491e7a5808da687c
bruskajp <bruskajp@clarkson.edu>
2016-07-02 19:53:16 -0400

    caesarCipher h and cpp

---
+++ EncryptioGuiCodeBlocks/caesarCipher.cpp
@@ -0,0 +1,13 @@
+#include "caesarCipher.h"
+
+#include <iostream>
+
+using namespace std;
+
+int CaesarCipher::encrypt(){
+    cout << "I encrypted the thing: ";
+    output = "fdsa;lf;naocn";
+    cout << output << "\n";
+    return 0;
+};
+
---
+++ EncryptioGuiCodeBlocks/caesarCipher.h
@@ -0,0 +1,11 @@
+#include <string>
+
+using namespace std;
+
+class CaesarCipher{
+    public:
+        string input;
+        string output;
+        int encrypt();
+        int decrypt();
+};
